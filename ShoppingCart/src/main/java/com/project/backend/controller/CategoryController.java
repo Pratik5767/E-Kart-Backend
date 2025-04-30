20349c32a44da3af99c7df19e048316e7790df8c
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-04-30 12:03:46 +0530

    getAllCategories Controller created

--- ShoppingCart/src/main/java/com/project/backend/controller/CategoryController.java
+++ ShoppingCart/src/main/java/com/project/backend/controller/CategoryController.java
@@ -1,5 +1,34 @@
 package com.project.backend.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.backend.model.Category;
+import com.project.backend.response.ApiResponse;
+import com.project.backend.service.category.ICategoryService;
+
+import lombok.RequiredArgsConstructor;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;;
+
+@RestController
+@RequestMapping("${api.prefix}/categories")
+@RequiredArgsConstructor
 public class CategoryController {
 
+	private final ICategoryService categoryService;
+	
+	@GetMapping("/all")
+	public ResponseEntity<ApiResponse> getAllCategories() {
+		try {
+			List<Category> categories = categoryService.getAllCategories();
+			return ResponseEntity.ok(new ApiResponse("Found!", categories));
+		} catch (Exception e) {
+			return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error", INTERNAL_SERVER_ERROR));
+		}
+	}	
 }
